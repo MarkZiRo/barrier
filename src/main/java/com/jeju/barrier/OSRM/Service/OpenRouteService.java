f78e44563b3c10fb6e1b3d84f0383a83aea2ca16
MarkZiRo <yhj216@hanmail.net>
2024-11-30 11:03:56 +0900

    logging 추가

--- src/main/java/com/jeju/barrier/OSRM/Service/OpenRouteService.java
+++ src/main/java/com/jeju/barrier/OSRM/Service/OpenRouteService.java
@@ -1,15 +1,21 @@
 package com.jeju.barrier.OSRM.Service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Collections;
+
+@Slf4j
 @Service
 public class OpenRouteService {
+
     private static final String BASE_URL = "https://api.openrouteservice.org/v2/directions/foot-walking";
 
     @Value("${openroute.api.key}")
@@ -23,10 +29,18 @@ public class OpenRouteService {
                 .toUriString();
 
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "Bearer " + apiKey);
+        headers.set("Authorization",  apiKey);
 
         HttpEntity<?> entity = new HttpEntity<>(headers);
         RestTemplate restTemplate = new RestTemplate();
+
+        restTemplate.setInterceptors(Collections.singletonList((request, body, execution) -> {
+            log.info("Request URL: " + request.getURI());
+            log.info("Request Headers: " + request.getHeaders());
+            ClientHttpResponse response = execution.execute(request, body);
+            log.info("Response Status: " + response.getStatusCode());
+            return response;
+        }));
 
         return restTemplate.exchange(url, HttpMethod.GET, entity, Object.class).getBody();
     }
