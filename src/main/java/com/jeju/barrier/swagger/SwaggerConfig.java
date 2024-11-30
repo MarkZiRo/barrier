ee79b9bf2c09e1bdae1c4853c503ad5a0d1930e0
MarkZiRo <yhj216@hanmail.net>
2024-11-30 11:20:25 +0900

    swaager변경

--- src/main/java/com/jeju/barrier/swagger/SwaggerConfig.java
+++ src/main/java/com/jeju/barrier/swagger/SwaggerConfig.java
@@ -4,14 +4,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://43.202.62.146")
+                ))
                 .info(new Info().title("Barrier Free").version("1.0"))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new io.swagger.v3.oas.models.Components()
